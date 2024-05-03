86b5ef7466b427c25c2d6ca0244fa5e0dfd5091f
arunkumar261 <arunkumarreddy261@gmail.com>
2024-05-03 22:36:51 +0530

    developer pushes author feature with webhook feature

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const logger = pino({
 const expLogger = expPino({
     logger: logger
 });
-//adding for version change and automatic deployment
+//adding for version change and automatic deployment in jenkins pipeline
 // MongoDB
 var db;
 var collection;
