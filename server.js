49eca7f1bef4250ed2577059bd2d6e5ed9ab997f
arunkumar261 <arunkumarreddy261@gmail.com>
2024-05-08 18:16:54 +0530

    chenged some content to raise PR req from feature-1 to main

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK SUCCESSFULL',
+        app: 'OKK',
         mongo: mongoConnected
     };
     res.json(stat);
